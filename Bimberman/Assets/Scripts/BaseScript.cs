867dff9a436e88f290960a0b382ddc0a4094296d
Weronika Kowalkowska <wkow19@gmail.com>
2026-03-11 23:19:04 +0100

    inventory panel on I in base

--- Bimberman/Assets/Scripts/BaseScript.cs
+++ Bimberman/Assets/Scripts/BaseScript.cs
@@ -2,5 +2,33 @@ using UnityEngine;
 
 public class BaseScript : MonoBehaviour
 {
+    public GameObject InventoryText;
+    public GameObject UI;
+
+    void Awake()
+    {
+        UI.SetActive(false);
+        //InventoryText.text += "";
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (UI.activeSelf)
+            {
+                UI.SetActive(false);
+            }
+            else
+            {
+                UI.SetActive(true);
+            }
+        }
+    }
 
 }
